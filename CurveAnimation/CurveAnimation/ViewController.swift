839728d7fc796f7f4dcde056081b74267eee716b
Badarinath Venkatnarayansetty <Badarinath_Venkatnarayansetty@intuit.com>
2020-02-01 18:37:25 -0800

    Initial commit project creation

---
+++ CurveAnimation/CurveAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurveAnimation
+//
+//  Created by Venkatnarayansetty, Badarinath on 2/1/20.
+//  Copyright © 2020 Venkatnarayansetty, Badarinath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
