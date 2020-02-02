502b5bd907cdb49bd6e897ea375b9bcb43644a74
Badarinath Venkatnarayansetty <Badarinath_Venkatnarayansetty@intuit.com>
2020-02-01 22:11:07 -0800

    Empty Cart feature

--- CurveAnimation/CurveAnimation/ViewController.swift
+++ CurveAnimation/CurveAnimation/ViewController.swift
@@ -32,13 +32,22 @@ class ViewController: UIViewController {
         imageView.image = UIImage(named: "credit_card")
         return imageView
     }()
+    
+    lazy var emptyCart:UIButton = {
+           let button = UIButton(type: UIButton.ButtonType.system)
+           button.translatesAutoresizingMaskIntoConstraints = false
+           button.setTitle("Empty Cart", for: UIControl.State.normal)
+           //button.setTitleColor(UIColor.blue, for: UIControl.State.normal)
+           button.addTarget(self, action: #selector(emptyCart(_:)), for: UIControl.Event.touchUpInside)
+           return button
+       }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor.white
         
-        [button, cartImage, cardImage].forEach { (view) in
+        [button, cartImage, cardImage, emptyCart].forEach { (view) in
             self.view.addSubview(view)
         }
         
@@ -50,15 +59,22 @@ class ViewController: UIViewController {
             self.button.heightAnchor.constraint(equalToConstant: 40),
             self.button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
             
-            self.cardImage.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: -80),
-            self.cardImage.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            //self.cardImage.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: -80),
+            //self.cardImage.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            self.cardImage.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 40),
+            self.cardImage.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 150),
             self.cardImage.widthAnchor.constraint(equalToConstant: 40),
             self.cardImage.heightAnchor.constraint(equalToConstant: 40),
             
             self.cartImage.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
-            self.cartImage.bottomAnchor.constraint(equalTo: self.view.bottomAnchor,constant: -50),
+            self.cartImage.bottomAnchor.constraint(equalTo: self.view.bottomAnchor,constant: -30),
             self.cartImage.widthAnchor.constraint(equalToConstant: 40),
             self.cartImage.heightAnchor.constraint(equalToConstant: 40),
+            
+            self.emptyCart.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 150),
+            self.emptyCart.bottomAnchor.constraint(equalTo: self.view.bottomAnchor,constant: -20),
+            self.emptyCart.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10),
+            self.emptyCart.heightAnchor.constraint(equalToConstant: 40)
         ])
     }
 
@@ -88,6 +104,17 @@ class ViewController: UIViewController {
         animationGroup.duration = 1.0
         self.cardImage.isHidden = false
         cardImage.layer.add(animationGroup, forKey: "curvedAnimation")
+    }
+    
+    @objc func emptyCart(_ sender: UIButton) {
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.repeatCount = 2
+        animation.duration = 0.06
+        animation.autoreverses = true
+        animation.fromValue = CGPoint(x: cartImage.center.x - 5, y: cartImage.center.y)
+        animation.toValue = CGPoint(x: cartImage.center.x + 5, y: cartImage.center.y)
+        cartImage.layer.add(animation, forKey: "position")
+        cartImage.image = UIImage(named: "cart")
     }
 
 }
